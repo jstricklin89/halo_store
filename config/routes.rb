e4af64e6a1cc0d427613627e583440aea4944176
jstricklin89 <jstricklin89@gmail.com>
2018-10-01 17:23:49 -0500

    done i think

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :items
   resources :carts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
 end
