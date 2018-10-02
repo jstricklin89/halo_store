2ab8c2ae73e36bc355a9f096a4a796744ce8a103
jstricklin89 <jstricklin89@gmail.com>
2018-10-02 15:16:13 -0500

    god damn it

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,33 +1,29 @@
 class ItemsController < ApplicationController
 
     def index
-        # if params[:name]
+        if params[:name]
+            @items = Item.where('name LIKE ?', "%{params[:name]}%")
+        else
         @items = Item.all
+        end
     end
 
     def show
         @item = Item.find(params[:id])
     end
 
-    def search
+    # def search
         # if params[:category_id]
         #     @items = Item.all.select do |item|
         #         item.category_id == params[:category_id].to_i
         #     end
-        @items = Item.all.select do |item|
+    #     @items = Item.all.select do |item|
             
-        if params[:query]
-                item.name.downcase == params[:query].downcase
-            end
-        end
-        render :index
-    end
-
-
-    private
-
-    def item_params
-
-    end
+    #     if params[:query]
+    #             item.name.downcase == params[:query].downcase
+    #         end
+    #     end
+    #     render :index
+    # end
 end
 
