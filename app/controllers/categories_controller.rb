178819b7ca9c9ce3115a7ab3f449623901ce4901
Naeblis <johnmlowery@gmail.com>
2018-10-02 12:44:06 -0500

    cart work

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,22 @@
+class CategoriesController < ApplicationController
+    
+    
+    def index
+        @categories = Category.all 
+
+    end
+    
+    
+    def new
+        
+    end
+    
+    
+    def create
+        
+    end
+    
+    def show
+        @category = Category.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :items
   resources :carts
   resources :users
+  resources :categories
   root to: 'pages#index'
   root 'application#hello'
   get '/login' => 'users#new'
