5f11469fcf2715ef3f4ffa01a055d5e14375339e
Naeblis <johnmlowery@gmail.com>
2018-10-05 08:51:50 -0500

    stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   # before_action :redirect_to_login_page_if_not_logged_in, except: [:new, :create]
   def index
-    if !session[:user_id] > 0
-      session[:user_id] = nil
-    end
+    # if !session[:user_id] = nil
+    #   session[:user_id] = nil
+    # end
     @categories = Category.all 
     @items = Item.all
   end
